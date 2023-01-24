012af6fc96fb2512cb8151fb88eb7f3b4ff06efc
Mintaek2 <789rkd@naver.com>
2023-01-24 23:25:56 +0900

    feat : Header 추가

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -1,11 +1,36 @@
 import React from "react";
-
-const Header = () =>{
-    return (
-        <>
-        머리
-        </>
-    )
-}
+import styled from "styled-components";
+const Header = () => {
+  return (
+    <>
+      <Container>
+        <Content>ABOUT</Content>
+        <Content>SKILLS</Content>
+        <Title>MINTAEK</Title>
+        <Content>PROJECT</Content>
+        <Content>CONTACT</Content>
+      </Container>
+    </>
+  );
+};
 
 export default Header;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 12vh;
+  display: flex;
+  gap: 4rem;
+  align-items: center;
+  justify-content: center;
+  color: wheat;
+`;
+
+const Content = styled.div`
+    font-size: 20px;
+`;
+
+const Title = styled.div`
+    font-weight: bold;
+    font-size: 50px;
+`;
