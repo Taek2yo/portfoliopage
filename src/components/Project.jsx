7b1d2692d8df3fa99f538ff340199555da5278c7
Mintaek2 <789rkd@naver.com>
2023-03-16 19:51:49 +0900

    refact : 안쓰는 코드 삭제

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useEffect } from "react";
-import styled, { keyframes } from "styled-components";
+import React, { useState } from "react";
+import styled from "styled-components";
 import Header from "./Header";
 import bg2 from "../assets/img/bg2.jpg";
 import purple from "../assets/img/onPurple.jpg";
